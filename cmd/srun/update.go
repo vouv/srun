6abdc5c7d977efc395bbe2fe919a4d42f7344cd2
monigo <monigo@aliyun.com>
2019-12-02 21:35:30 +0800

    优化结构

---
+++ cmd/srun/update.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const repo = "https://github.com/vouv/srun/releases/latest"
+const updateTimeout = 3 * time.Second
+
+var client = http.Transport{
+	DialContext: func(ctx context.Context, network, addr string) (conn net.Conn, e error) {
+		conn, err := net.DialTimeout(network, addr, updateTimeout)
+		if err != nil {
+			return nil, err
+		}
+		_ = conn.SetDeadline(time.Now().Add(updateTimeout))
+		return conn, nil
+	},
+}
+
+func HasUpdate() (ok bool, version string, dist string) {
+	req, err := http.NewRequest("GET", repo, nil)
+
+	if err != nil {
+		log.Debug("请求错误", err)
+		return
+	}
+	res, err := client.RoundTrip(req)
+	if err != nil {
+		log.Debug("请求错误", err)
+		return
+	}
+	dist = res.Header.Get("Location")
+	arr := strings.Split(dist, "/")
+	version = arr[len(arr)-1]
+
+	log.Debug("最新版本", version)
+
+	ok = version != Version
+	return
+
+}
